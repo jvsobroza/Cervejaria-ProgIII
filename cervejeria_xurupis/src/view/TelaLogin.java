d1a92a46a3cffe6b539a9de2238a6997d796da96
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2025-11-13 12:58:16 -0300

    Atualização da tela de login
    Adicionar um rodapé.

--- cervejeria_xurupis/src/view/TelaLogin.java
+++ cervejeria_xurupis/src/view/TelaLogin.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -8,10 +10,19 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JButton;
 
 public class TelaLogin extends JPanel {
 
@@ -19,6 +30,15 @@ public class TelaLogin extends JPanel {
 	private JPanel panel;
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JTextField txtEmail;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JTextField textField;
+	private JPanel panel_1;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
 
 	/**
 	 * Create the panel.
@@ -31,15 +51,74 @@ public class TelaLogin extends JPanel {
 	private void initComponents() throws IOException {
 		setBackground(new Color(230, 205, 153));
 		setBounds(100, 100, 1000, 600);
-		setLayout(new MigLayout("", "[grow][][][]", "[][]"));
+		setLayout(new MigLayout("", "[][350.00][grow][204.00][][grow]", "[][][][][][][grow]"));
 		BufferedImage img = null;
 
 		img = ImageIO.read(TelaLogin.class.getResource("/resources/img/logos/logoNBG.png")); 
 		Image dimg = img.getScaledInstance(250, 250,Image.SCALE_SMOOTH);
 		
+		this.panel_1 = new JPanel();
+		Border bordaSimples = BorderFactory.createLineBorder(Color.BLACK, 4);
+		panel_1.setBorder(bordaSimples);		
+		add(this.panel_1, "flowx,cell 1 0,alignx leading,aligny bottom");
+		this.panel_1.setLayout(new MigLayout("", "[][][][][][][]", "[][][]"));
+		
+		this.lblNewLabel_3 = new JLabel("Realizar login:");
+		this.panel_1.add(this.lblNewLabel_3, "cell 0 0 2 1");
+		this.lblNewLabel_3.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		
+		this.lblNewLabel_4 = new JLabel("Email:");
+		this.panel_1.add(this.lblNewLabel_4, "cell 0 1,alignx left");
+		this.lblNewLabel_4.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		this.lblNewLabel_5 = new JLabel("Senha:");
+		this.panel_1.add(this.lblNewLabel_5, "cell 0 2");
+		this.lblNewLabel_5.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		this.textField = new JPasswordField();
+		this.panel_1.add(this.textField, "cell 1 2 2 1");
+		this.textField.setColumns(10);
+		
+		this.txtEmail = new JTextField();
+		this.panel_1.add(this.txtEmail, "cell 1 1 3 1,growx,aligny center");
+		this.txtEmail.setText("Digite seu email");
+		this.txtEmail.setForeground(Color.GRAY);
+		this.txtEmail.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (txtEmail.getText().equals("Digite seu email")) {
+                    txtEmail.setText("");
+                    txtEmail.setForeground(Color.BLACK);
+                }
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				// TODO Auto-generated method stub
+				if (txtEmail.getText().isEmpty()) {
+					txtEmail.setText("Digite seu email");
+					txtEmail.setForeground(Color.GRAY); 
+                }
+            }
+			}
+		);
+		this.txtEmail.setColumns(10);
+		
+		this.lblNewLabel_2 = new JLabel("Seja bem vindo a melhor cervejaria da America Látina");
+		this.lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		add(this.lblNewLabel_2, "cell 0 0 4 1,alignx left,aligny top");
+		
 		this.lblNewLabel_1 = new JLabel("");
 		this.lblNewLabel_1.setIcon(new ImageIcon(dimg));
-		add(this.lblNewLabel_1, "cell 3 0,alignx right,aligny top");
+		add(this.lblNewLabel_1, "cell 5 0,alignx center,aligny top");
+		
+		this.btnNewButton_1 = new JButton("Realizar Login");
+		this.btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(this.btnNewButton_1, "flowx,cell 1 1,alignx right");
+		
+		this.btnNewButton = new JButton("Realizar Cadastro");
+		this.btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(this.btnNewButton, "cell 1 1,alignx right");
 	}
 
 }
